21481675fe6c383e80a8d5435fc7825d6262932c
Ivana Huckova <ivana.huckova@gmail.com>
2019-07-10 14:52:35 +0200

    project setup

--- basic_block_gp/blockchain.py
+++ basic_block_gp/blockchain.py
@@ -158,11 +158,10 @@ def mine():
     # The sender is "0" to signify that this node has mine a new coin
     # The recipient is the current node, it did the mining!
     # The amount is 1 coin as a reward for mining the next block
-    blockchain.new_transaction( sender="0", recipient=node_identifier, amount=1)
+    blockchain.new_transaction(sender="0", recipient=node_identifier, amount=1)
     # Forge the new Block by adding it to the chain
     previous_hash = blockchain.hash(last_block)
     block = blockchain.new_block(proof, previous_hash)
-
 
     # Send a response with the new block
     response = {
