1f7df8a94e1e69b95d0b3a59c5c69fffa7233b29
Ivana Huckova <ivana.huckova@gmail.com>
2019-07-11 14:16:42 +0200

    fix blockchain day 1 task

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -1,53 +1,53 @@
-import sys
 import hashlib
 import requests
-from flask import jsonify
-from blockchain import blockchain, node_identifier
+import time
+import json
+
+import sys
 
 
-# TODO: Implement functionality to search for a proof
+def valid_proof(last_proof, proof):
+    """
+    Validates the Proof:  Does hash(last_proof, proof) contain 4
+    leading zeroes?
+    """
+    guess = f'{last_proof}{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    return guess_hash[-5:] == "00000"
 
 
+# Implement functionality to search for a proof
 if __name__ == '__main__':
-    # What node are we interacting with?
     if len(sys.argv) > 1:
         node = sys.argv[1]
     else:
         node = "http://localhost:5000"
 
     coins_mined = 0
+    proof = 0
     # Run forever until interrupted
     while True:
-        # TODO: Get the last proof from the server and look for a new one
-        URL = "http://localhost:5000/last_proof"
-        r = requests.get(URL)
-        last_proof = r.json()["last_proof"]
+        # Get the last proof from the server and look for a new one
+        lp_response = requests.get(f'{node}/last_proof')
+        body = lp_response.json()
+        last_proof = body['last_proof']
 
-        print('PROOF OF WORK STATUS: MINING IN PROGRESS')
-        new_proof = blockchain.proof_of_work(last_proof)
-        print('PROOF OF WORK STATUS: MINING COMPLETE')
-        # print(last_proof)
-        # print(new_proof)
+        # Search new proof
+        print('Mining new block')
+        start_time = time.time()
+        valid = False
+        headers = {'Content-Type': 'application/json'}
+        while not valid:
+            # print(f"Checking for nonce: {proof}, last_nonce: {last_proof}")
+            valid = valid_proof(last_proof, proof)
+            proof += 1
+        res = requests.post(f'{node}/mine',
+                            data=json.dumps({'proof': proof, 'last_proof': last_proof}), headers=headers)
 
-        # TODO: When found, POST it to the server {"proof": new_proof}
-
-        proof_json = {
-            "proof": new_proof
-            # "client_node_identifier": node_identifier
-        }
-
-        payload = proof_json
-        MINE_URL = 'http://localhost:5000/mine'
-        mine_r = requests.post(MINE_URL, json=payload)
-        # print(mine_r)
-
-        # TODO: If the server responds with 'New Block Forged'
-        # add 1 to the number of coins mined and print it.  Otherwise,
-        # print the message from the server.
-        mine_r_json = mine_r.json()
-        if mine_r_json['message'] == 'New Block Forged':
-            coins_mined += 1
-            print(f"Coins mined: {coins_mined}")
-        # Otherwise, print the failure message from the server
-        else:
-            print(mine_r_json['failure'])
+        response = res.json()
+        end_time = time.time()
+        coins_mined += 1
+        proof = 0
+        print(
+            f'Block mined in {round(end_time-start_time, 2)} sec. Number of mined coins: {coins_mined}')
+        print(f"Block number: {response['index']}\n")
